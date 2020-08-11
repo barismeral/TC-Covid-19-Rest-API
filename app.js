64e97a6ee1fb7f7a184017012bbbe0f1c7f4b27c
Barış Meral <barismeral.net@hotmail.com>
2020-08-11 22:54:30 +0300

    changed PORT

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
+app.set('port', process.env.PORT || 5000);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
